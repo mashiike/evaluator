3ef86c330328f60440343006f688be70d0ab1c6a
mashiike <ikeda-masashi@kayac.com>
2021-11-03 13:21:06 +0900

    more example

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package evaluator_test
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/mashiike/evaluator"
 )
@@ -14,4 +15,24 @@ func ExampleEvaluator_Eval() {
 
 	// Output:
 	// 3
+}
+
+func ExampleComparator_Compare() {
+
+	e, err := evaluator.New("(var1 + 0.5) <= var2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	c, ok := e.AsComparator()
+	if !ok {
+		log.Fatal("not comparative expr")
+	}
+	ans, err := c.Compare(evaluator.Variables{"var1": 0.5, "var2": 3})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ans)
+
+	// Output:
+	// true
 }
